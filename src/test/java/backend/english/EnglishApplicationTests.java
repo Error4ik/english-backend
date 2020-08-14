615fcb9e95501cfc9da46c0c876b9f7b295e7176
Error4ik <fearerror@yandex.ru>
2020-08-14 16:02:07 +0300

    Initial commit

---
+++ src/test/java/backend/english/EnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.english;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
