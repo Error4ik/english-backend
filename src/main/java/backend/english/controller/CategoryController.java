0487f84046010fed1da1d37697fc6eb85122d825
Error4ik <fearerror@yandex.ru>
2020-08-19 23:51:46 +0300

    Added the ability to add, update, and delete categories.

--- src/main/java/backend/english/controller/CategoryController.java
+++ src/main/java/backend/english/controller/CategoryController.java
@@ -3,11 +3,15 @@ package backend.english.controller;
 import backend.english.domain.Category;
 import backend.english.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("category")
@@ -23,5 +27,25 @@ public class CategoryController {
     @RequestMapping("/categories")
     public ResponseEntity<List<Category>> getCategories() {
         return ResponseEntity.ok(this.categoryService.getCategories());
+    }
+
+    @RequestMapping("/add")
+    public ResponseEntity<Category> addCategory(@RequestBody @NonNull Category category) {
+        return ResponseEntity.ok(this.categoryService.saveCategory(category));
+    }
+
+    @RequestMapping("/update")
+    public ResponseEntity<Category> updateCategory(@RequestBody @NonNull Category category) {
+        return ResponseEntity.ok(this.categoryService.updateCategory(category));
+    }
+
+    @RequestMapping("/delete/{id}")
+    public ResponseEntity deleteCategory(@PathVariable UUID id) {
+        try {
+            this.categoryService.deleteCategory(id);
+        } catch (EmptyResultDataAccessException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
