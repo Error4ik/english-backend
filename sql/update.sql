f81e5022f02156fb6c3635b4c4c318384d4fb2f5
Error4ik <fearerror@yandex.ru>
2020-11-18 21:36:12 +0300

    Create a trigger to update the category when working with the card. #2

--- sql/update.sql
+++ sql/update.sql
@@ -1,1 +1,21 @@
 ALTER TABLE cards DROP COLUMN transcription;
+
+CREATE OR REPLACE FUNCTION changes_card() RETURNS TRIGGER AS
+$$
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        UPDATE categories SET number_of_cards = number_of_cards - 1 where old.category_id = id;
+        RETURN OLD;
+    ELSIF (TG_OP = 'INSERT') THEN
+        UPDATE categories SET number_of_cards = number_of_cards + 1 where new.category_id = id;
+        RETURN NEW;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER changes
+    AFTER INSERT OR DELETE
+    ON cards
+    FOR EACH ROW
+EXECUTE PROCEDURE changes_card();
