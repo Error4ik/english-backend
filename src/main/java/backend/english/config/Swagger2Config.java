0fc81df29eaa71e75854bc0c22f6b6b4d84c5ba7
Error4ik <fearerror@yandex.ru>
2020-12-14 00:51:04 +0300

    Simple configure swagger. #3

--- src/main/java/backend/english/config/Swagger2Config.java
+++ src/main/java/backend/english/config/Swagger2Config.java
@@ -3,9 +3,12 @@ package backend.english.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -19,6 +22,18 @@ public class Swagger2Config {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("backend.english.controller"))
                 .paths(regex("/.*"))
-                .build();
+                .build().apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "English API",
+                "Simple API for english tutorial",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("Alex", "http://english.com", "english@english.com"),
+                "API License",
+                "http://english.com",
+                Collections.emptyList());
     }
 }
--- src/main/java/backend/english/controller/CardController.java
+++ src/main/java/backend/english/controller/CardController.java
@@ -3,6 +3,7 @@ package backend.english.controller;
 import backend.english.domain.Card;
 import backend.english.service.CardService;
 import backend.english.util.SearchParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,17 +28,20 @@ public class CardController {
     }
 
     @GetMapping("/cards")
+    @ApiOperation(value = "Finds all cards.", notes = "Finds all cards from database.")
     public ResponseEntity<List<Card>> getCards() {
         return ResponseEntity.ok(this.cardService.getCards());
     }
 
     @PostMapping("/find-cards-by-parameters")
+    @ApiOperation(value = "Look up specific cards by parameters.", notes = "Finds cards by parameters.")
     public ResponseEntity<Page<Card>> findCardByParameters(@RequestBody @NonNull SearchParams searchParams) {
         logger.info(String.format("Input arguments: %s", searchParams));
         return ResponseEntity.ok(this.cardService.findCardByParams(searchParams));
     }
 
     @PostMapping("/add")
+    @ApiOperation(value = "Save new Card.", notes = "Save new Card to database.")
     public ResponseEntity<Card> addCard(@RequestBody @NonNull Card card) {
         logger.info(String.format("Input arguments: %s", card));
         Card c = this.cardService.saveCard(card);
